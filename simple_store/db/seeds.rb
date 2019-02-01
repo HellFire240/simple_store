0782ab1f706943a3e3046e62bff0a48a90971cde
iflight <ian.df@hotmail.com>
2019-02-01 17:45:14 -0600

    Created seed script.

--- simple_store/db/seeds.rb
+++ simple_store/db/seeds.rb
@@ -8,4 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(description: 'The first one')
+676.times do
+  Product.create(title: Faker::Cannabis.strain, description: Faker::Cannabis.health_benefit, price: Faker::Number.decimal(2), stock_quantity: Faker::Number.between(0, 100))
+end
