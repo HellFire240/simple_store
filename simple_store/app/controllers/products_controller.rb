cb88a8dbd74701643bbc66ef7dd4e69f74b07d49
iflight <ian.df@hotmail.com>
2019-02-01 18:27:36 -0600

    Added instance variables to products controller. Modified index view.

--- simple_store/app/controllers/products_controller.rb
+++ simple_store/app/controllers/products_controller.rb
@@ -3,5 +3,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.order(:id)
+    @count = Product.count
+    @avg_price = Product.average(:price).round(2)
+    @total_stock = Product.sum(:stock_quantity)
   end
 end
